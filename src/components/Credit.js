e5225705fdfa8d263c7044bc2640b1636d8bad92
haripreeth369 <haripreeththota@gmail.com>
2024-06-29 02:52:36 +0530

    save

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Made by
+      <a href="https://www.linkedin.com/in/haripreeth-thota/"
+        > Haripreeth Thota</a
+      >, <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
